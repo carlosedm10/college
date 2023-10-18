ed8aed4998fc5ebcb1dd11859e7db960cf898543
Carlos Eduardo <carlosedm@hotmail.es>
2023-10-18 11:10:32 +0200

    files

--- Econometria/Prac4/Prac.py
+++ Econometria/Prac4/Prac.py
@@ -1,50 +1,90 @@
+import xml.etree.ElementTree as ET
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
-from scipy import stats
+import seaborn as sns
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
-# Lectura de datos
-df_gretl = pd.read_csv("/mnt/data/MRL028tc.csv", sep=";", skiprows=3)
 
-# Ajuste del modelo inicial con 'PARQUE' como predictor
-X_initial = df_gretl["PARQUE"].astype(float)
-y_initial = df_gretl["MUERTOS"].astype(float)
-X_initial = sm.add_constant(X_initial)
-model_initial = sm.OLS(y_initial, X_initial).fit()
+# Parse the XML data
+tree = ET.parse("/Users/carlosedm10/projects/college/Econometria/Prac4/MRL028tc.gdt")
+root = tree.getroot()
 
-# Creación de la variable de interacción
-df_gretl["ACCIDENTESxPARQUE"] = df_gretl["ACCIDENTES"].astype(float) * df_gretl[
-    "PARQUE"
-].astype(float)
+# Extracting variable names from the XML
+variable_names = [variable.get("name") for variable in root.findall(".//variable")]
 
-# Ajuste del modelo con interacción
-X = df_gretl[["PARQUE", "ACCIDENTESxPARQUE"]].astype(float)
+# Extracting all the data from the "obs" tags
+all_obs_data = [obs.text.split() for obs in root.findall(".//observations/obs")]
+
+# Convert the extracted data to a DataFrame using the variable names as columns
+df = pd.DataFrame(all_obs_data, columns=variable_names)
+
+# Convert to appropriate data types
+df = df.apply(pd.to_numeric)
+
+df.head()
+
+# print(df)
+
+
+# Define dependent variable y and independent variables X
+y = df["PARQUE"]
+X = df.drop("PARQUE", axis=1)
+
+print(X.columns)
+
+# Add a constant (intercept) to the independent variables
 X = sm.add_constant(X)
-model_interaction = sm.OLS(y, X).fit()
 
-# QQ-plot de los residuos del modelo con interacción
-residuals_revised = model_interaction.resid
-stats.probplot(residuals_revised, dist="norm", plot=plt)
-plt.title("QQ-plot de los residuos del modelo revisado")
+# Calculate VIF for each independent variable
+vif_data = pd.DataFrame()
+vif_data["Variable"] = X.columns
+vif_data["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+
+print(vif_data)
+
+
+# Fit the linear regression model
+model = sm.OLS(y, X).fit()
+
+# Get the summary of the regression
+summary = model.summary()
+summary
+print(summary)
+
+# Calculating residuals
+residuals = model.resid
+fitted = model.fittedvalues
+
+# Plotting residuals vs fitted values
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x=fitted, y=residuals)
+plt.axhline(y=0, color="red", linestyle="--")
+plt.title("Residuals vs Fitted Values")
+plt.xlabel("Fitted Values")
+plt.ylabel("Residuals")
 plt.show()
 
-# Gráficos de residuos para el modelo con interacción
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 15))
-axes[0].scatter(
-    model_interaction.fittedvalues, residuals_revised, color="blue", edgecolor="k"
-)
-axes[0].set_title("Residuos vs Valores Estimados")
-axes[1].scatter(
-    range(len(residuals_revised)), residuals_revised, color="blue", edgecolor="k"
-)
-axes[1].set_title("Residuos vs Posición en la tabla")
-axes[2].scatter(
-    df_gretl["PARQUE"].astype(float), residuals_revised, color="blue", edgecolor="k"
-)
-axes[2].set_title("Residuos vs PARQUE")
-plt.tight_layout()
+
+# Histogram of residuals
+plt.figure(figsize=(10, 6))
+sns.histplot(residuals, kde=True, bins=20)
+plt.title("Histogram of Residuals")
+plt.xlabel("Residual")
+plt.ylabel("Frequency")
 plt.show()
 
-# Modelo con transformación logarítmica
-y_log_revised = np.log(df_gretl["MUERTOS"].astype(float))
-model_log_revised = sm.OLS(y_log_revised, X).fit()
+# Q-Q plot for residuals
+plt.figure(figsize=(10, 6))
+sm.qqplot(residuals, line="45", fit=True)
+plt.title("Q-Q Plot of Residuals")
+plt.show()
+
+# Plot residuals over time
+plt.figure(figsize=(12, 6))
+plt.plot(df.index, residuals, marker="o", linestyle="--")
+plt.axhline(y=0, color="red", linestyle="-")
+plt.title("Residuals over Time")
+plt.xlabel("Time")
+plt.ylabel("Residual")
+plt.show()
