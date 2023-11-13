d6ede6c55b6694483361d73d615ace63647f0699
oscarjibou <oscarjibou@gmail.com>
2023-11-13 10:57:54 +0100

    nothing

--- Econometria/Plantilla_A.py
+++ Econometria/Plantilla_A.py
@@ -1,38 +1,106 @@
 """
-1. Escribir el Modelo por cada nivel de sector
+1. Escribir el Modelo
 2. El modelo es adecuado → Estadistico + prueba + conclusion
-3. Determina si la relación entre Y y X no depende del factor 
+3. Determina si la relación entre Y y X no depende del factor (colinealidad)
 4. Determina si existen problemas de heterocedasticidad
 5. Determine la forma en que causa la desviacion tipica del error (interpreta valores)
 """
 
+from numpy import mean
 import pandas as pd
 import statsmodels.api as sm
+from scipy.stats import f
+from utilities import backward_elimination
 
-from utilities import backward_elimination, compare_models, forward_selection
+# Significance level
+threshold = 0.05
 
 
 # Load the CSV file
 file_path = "Econometria/MRL016-1.csv"
 data = pd.read_csv(file_path)
 
-# Setting up the independent and dependent variables
-X = data[["EMPLEOS_AGR"]]  # Predictor
-y = data["VAA_AGR"]  # Dependent variable
+print(data.head())  # print the first 5 rows to see the data for building the model
+
+########################################## 1. Escribir el Modelo ##########################################
 
 # Creating dummy variables for the 'NPROV' column
-nprov_dummies = pd.get_dummies(data["NPROV"])
+dummies = pd.get_dummies(
+    data["NPROV"], drop_first=True, dtype=int
+)  # drop_first=True to get K-1 dummies out of K categorical levels by removing the first one which is redundant
 
-# Joining the dummy variables with the original data
-data_with_dummies = data.join(nprov_dummies)
-X_with_dummies = data_with_dummies.drop(["VAA_AGR", "NPROV"], axis=1)
+data = data.drop(["NPROV"], axis=1)  # drop the 'NPROV' column
 
-# Comparing the two models using the compare_models function
-best_model = compare_models(X, y)
+# --------------------------- Dummy variables ---------------------------#
+# print(dummies.head())
 
-# Printing the best model method and summary
-print(best_model.summary())
+data["CASTELLON"] = dummies["CASTELLÓN"]  # interaction variable
+data["VALENCIA"] = dummies["VALENCIA"]  # interaction variable
+
+# --------------------------- Adjusted variables ---------------------------#
+data["EMPLEOS_AGR_centered"] = data["EMPLEOS_AGR"] - mean(
+    data["EMPLEOS_AGR"]
+)  # variable independiente
+
+# --------------------------- Interaction variables ---------------------------#
+
+data["EMPLEOS_CASTELLON"] = (
+    data["EMPLEOS_AGR"] * dummies["CASTELLÓN"]
+)  # interaction variable
+data["EMPLEOS_VALENCIA"] = (
+    data["EMPLEOS_AGR"] * dummies["VALENCIA"]
+)  # interaction variable
+
+# model_data = pd.concat([data, dummies])  # concatenate the data and dummies dataframes
+
+X = data[
+    [
+        "EMPLEOS_AGR_centered",
+        "CASTELLON",
+        "VALENCIA",
+        "EMPLEOS_CASTELLON",
+        "EMPLEOS_VALENCIA",
+    ]
+]  # variables independientes
+
+X = sm.add_constant(X)  # add a constant to the model
+y = data["VAA_AGR"]  # variable dependiente
 
 model = sm.OLS(y, X).fit()  # ordinary least squares model
+print(model.summary())  # print the model summary
 
-print(model.summary())
+########################################### 2. El modelo es adecuado ##########################################
+
+print("Deleting the non-significant variables:")
+new_model = backward_elimination(X, y, threshold)
+
+print(new_model.summary())
+
+# Degrees of freedom for the model (number of predictors) and residuals (sample size - number of predictors - 1)
+df_model = len(X.columns)
+df_residuals = len(X) - df_model - 1
+
+f_statistic = new_model.fvalue
+f_critical = f.ppf(1 - threshold, df_model, df_residuals)
+
+if f_statistic > f_critical:
+    print(f"Fcalc = {f_statistic} > Fcrit = {f_critical}.The model is adequate.")
+else:
+    print(f"Fcalc = {f_statistic} < Fcrit = {f_critical}.The model is not adequate.")
+
+
+########################################### * PREDICCIÓN * ##########################################
+new_model_params = new_model.params
+exog_data = {
+    "const": 1,  # Include the constant term
+    "EMPLEOS_AGR_centered": [100, 250],  # Example value
+    "VALENCIA": [0, 1],  # Example value (1 or 0)
+    "EMPLEOS_CASTELLON": [100, 0],  # Example value (EMPLEOS_AGR * CASTELLÓN)
+    "EMPLEOS_VALENCIA": [0, 150],  # Example value (EMPLEOS_AGR * VALENCIA)
+}  # This is the data given for the prediction
+exog_df = pd.DataFrame(exog_data)
+predicted_values = new_model.predict(exog=exog_df)
+
+for i in range(len(predicted_values)):
+    print(f"The {i+1}º predicted value for {y.name} is: {predicted_values[i]}")
+#hol
