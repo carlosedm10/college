0f1627f95a8a39a4cfd53da58c47ea338a14097b
Carlos Eduardo <carlosedm@hotmail.es>
2023-12-10 09:55:22 +0100

    cleaning

--- Econometria/Prac10.py
+++ Econometria/Prac10.py
@@ -7,10 +7,7 @@
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
 
-
-from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from pmdarima import auto_arima
 from matplotlib import pyplot as plt
