fac665d9409fb5f48f574e78238ab37ee27cef47
Carlos Eduardo <carlosedm@hotmail.es>
2024-03-20 22:06:32 +0100

    Prac 2 finished

--- TDS/utils.py
+++ TDS/utils.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
 from numpy.typing import ArrayLike
 from typing import Sequence, Optional
+from scipy.signal import get_window
+import numpy as np
 
 
 def continuous_time_plot(
@@ -11,7 +13,8 @@ def continuous_time_plot(
     save_path=None,
     line_style="-",  # Default line style
     linewidth=2,  # Adjust the width of the line
-    vertical_lines=None,
+    principal_lines=None,
+    secondary_lines=None,
 ):
     """
     This function plots a time series plot with the following characteristics:
@@ -47,9 +50,13 @@ def continuous_time_plot(
     plt.xticks(rotation=45)
     plt.tight_layout()
 
-    if vertical_lines is not None:
-        for line in vertical_lines:
+    if principal_lines is not None:
+        for line in principal_lines:
             plt.axvline(x=line, color="r", linestyle="--")
+
+    if secondary_lines is not None:
+        for line in secondary_lines:
+            plt.axvline(x=line, color="b", linestyle="--")
 
     if save_path is None:
         plt.show()
@@ -108,3 +115,41 @@ def discrete_time_plot(
         plt.show()
     else:
         plt.savefig(f"{save_path}/{variable_name} time plot.png")
+
+
+def split_signal_into_frames(
+    signal, sample_rate, window_size, window_overlap, window_type="hann"
+):
+    """
+    Split the signal into frames using a sliding window approach
+    Args:
+    signal (np.array): The input signal
+    sample_rate (int): The sample rate of the signal
+    window_size (float): The size of the window in seconds
+    window_overlap (float): The overlap between consecutive windows in seconds
+    window_type (str): The type of window to use
+
+    Returns:
+    np.array: The signal split into frames
+    """
+    window_length = int(window_size * sample_rate)
+    step_size = window_length - int(window_overlap * sample_rate)
+    window = get_window(window_type, window_length)
+    num_frames = int(np.ceil(float(np.abs(len(signal) - window_length)) / step_size))
+
+    # Zero padding at the end to make sure that all frames have equal number of samples
+    # without truncating any part of the signal
+    pad_signal_length = num_frames * step_size + window_length
+    z = np.zeros((pad_signal_length - len(signal)))
+    pad_signal = np.append(signal, z)  # Pad signal
+
+    indices = (
+        np.tile(np.arange(0, window_length), (num_frames, 1))
+        + np.tile(np.arange(0, num_frames * step_size, step_size), (window_length, 1)).T
+    )
+    frames = pad_signal[indices.astype(np.int32, copy=False)]
+
+    # Apply the window function to each frame
+    windowed_frames = frames * window
+
+    return windowed_frames
