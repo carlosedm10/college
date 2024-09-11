b939e277aac2d21e99d8203204ee181a8b0f56d5
carlosedm10 <carlosedm@hotmail.es>
2024-09-11 16:05:43 +0200

    --wip-- [skip ci]

--- DST/Prac1/UDP_client_v2.py
+++ DST/Prac1/UDP_client_v2.py
@@ -10,7 +10,7 @@ while communication_active:
     client_sock.sendto(message.encode(), (UDP_ADDRESS, UDP_PORT))
 
     modified_message, server_address = client_sock.recvfrom(2048)
-    print("(Client) Response from server :", modified_message.decode())
+    print("(Client) Response recieved :", modified_message.decode())
 
     if message.lower() == "end" or modified_message.decode() == "END":
         communication_active = False
