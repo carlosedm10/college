8778efb00b27fba512f064e16d4e07a4dd87543c
carlosedm10 <carlosedm@hotmail.es>
2024-09-12 18:42:17 +0200

    pract finished

--- DST/Prac1/final_tcp_client.py
+++ DST/Prac1/final_tcp_client.py
@@ -1,14 +1,11 @@
 # Constants for the server's address and port
 import base64
 import socket
-
 import pyotp
-
 from constants import DNI
 
-
-SERVER_ADDRESS = "127.0.0.1"
-SERVER_PORT = 19000
+SERVER_ADDRESS = "158.42.32.220"
+SERVER_PORT = 21000
 server_responded = False
 
 # Create a TCP/IP socket
@@ -17,24 +14,26 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to the server using the specified address and port
 client_socket.connect((SERVER_ADDRESS, SERVER_PORT))
 
+# Generate TOTP secret using the DNI
 secret = base64.b32encode(bytearray(DNI, "ascii")).decode("utf-8")
-
 totp_object = pyotp.TOTP(secret)
 qr_text = totp_object.provisioning_uri(name="mi_usuario", issuer_name="Mi App")
 
+# Prepare the message
+message = f"Carlos Eduardo Dominguez Martinez#{DNI}#"
 
-message = f"Carlos Eduardo Dominguez Martinez#{DNI}#" + qr_text, "utf-8"
+# Encode the message into bytes before sending it
+client_socket.send(message.encode("utf-8"))
 
-client_socket.send(bytes(message))
-
-while server_responded:
+# Wait for the server to respond
+while not server_responded:
     # Receive the message from the server (up to 1024 bytes)
     received_message = client_socket.recv(1024)
 
     # Decode and print the received message
-    print(received_message.decode())
+    print(received_message.decode("utf-8"))
 
-    # Close the connection to the server
+    # If a message was received, mark server_responded as True
     if received_message:
         server_responded = True
 
