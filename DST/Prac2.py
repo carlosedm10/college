72eb29578ac843308e6622cd489e0e75d974a3a3
carlosedm10 <carlosedm@hotmail.es>
2024-11-16 19:32:09 +0100

    --wip-- [skip ci]

--- DST/Prac2.py
+++ DST/Prac2.py
@@ -30,17 +30,17 @@ def on_message(client: mqtt.Client, _, message: mqtt.MQTTMessage):
 
 
 # Sending th DNI
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.username_pw_set(username="dst", password="dst")
-client.connect(BROKER_IP, PORT)
+cliente = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+cliente.username_pw_set(username="dst", password="dst")
+cliente.connect(BROKER_IP, PORT)
 print("conectado al Broker: ", BROKER_IP)
 
-client.publish(PETICION, DNI)
+cliente.publish(PETICION, DNI)
 print("publicado el mensaje: ", DNI, " en el topic: ", PETICION)
 
 # Subscribing to the topic where the broker will send the code
-client.subscribe(CODIGO)
-client.on_message = on_message
+cliente.subscribe(CODIGO)
+cliente.on_message = on_message
 
 
-client.loop_forever()
+cliente.loop_forever()
